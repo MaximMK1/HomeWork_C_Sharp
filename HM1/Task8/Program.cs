491bfa1f9bf9cb86bf57838111ea4ceafb6e1674
maxim kovalenko <mk@system-pro.net>
2023-01-19 22:03:18 +0300

    Первая домашка по С#

---
+++ HM1/Task8/Program.cs
@@ -0,0 +1,11 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int index = 2;
+while (index <= N)
+{
+    if ((index % 2) == 0) Console.Write(index + ", ");
+    index = index + 1;
+}
+Console.Write("\b\b  ");

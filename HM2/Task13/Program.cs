dcc8b3a5eee96993cb82ca8faa4fd8791cce62ee
maxim kovalenko <mk@system-pro.net>
2023-01-23 22:30:41 +0300

    Вторая домашка по С#

---
+++ HM2/Task13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine()); 
+if (num > 99 ) 
+{
+    while (num > 999) 
+   {
+    num = num / 10;
+   }
+num = num % 10;
+Console.Write ("Третья цифра {0}", num);
+}
+else
+Console.Write("Третьей цифры нет");

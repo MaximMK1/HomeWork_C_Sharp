4c10eec480bfd58debef29f2f7fd04a5fd40d4c6
maxim kovalenko <mk@system-pro.net>
2023-01-25 21:39:04 +0300

    Третья домашка по С#

---
+++ HM3/Task19/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num <= 99999) 
+{
+   int ind0 = num / 10000; 
+   int ind1 = ((num / 1000) % 10);
+   int ind3 = ((num % 100) / 10);  
+   if (ind0 == num % 10 && ind1 == ind3)
+   {
+    Console.WriteLine($"Число {num} является палиндромом");
+   } 
+   else
+   Console.WriteLine($"Число {num} не является палиндромом");
+}
+else Console.WriteLine("Введено неверное число");
+
+
---
+++ HM3/Task21/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и
+//  находит расстояние между ними в 3D пространстве.
+
+Console.WriteLine("Введите координаты точки A: ");
+Console.Write("x: ");
+int Ax = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int Ay = int.Parse(Console.ReadLine());
+Console.Write("z: ");
+int Az = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Введите координаты точки В: ");
+Console.Write("x: ");
+int Bx = int.Parse(Console.ReadLine());
+Console.Write("y: ");
+int By = int.Parse(Console.ReadLine());
+Console.Write("z: ");
+int Bz = int.Parse(Console.ReadLine());
+
+int diffX = Ax-Bx;
+int diffY = Ay-By;
+int diffZ = Az-Bz;  
+
+double S = Math.Sqrt(Math.Pow(diffX, 2) + Math.Pow(diffY, 2) + Math.Pow(diffZ, 2));
+Console.Write("Расстояние между точками равно {0}", Math.Round(S, 2));

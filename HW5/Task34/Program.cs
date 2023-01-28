8831001e60ae0feca1112a8b8c9d0a6aeccd20a9
Maxim Kovalenko <maxim.kova1enko@ya.ru>
2023-01-28 21:53:34 +0300

    + HW5 (Task 34, 35)

---
+++ HW5/Task34/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задайте массив заполненный случайными положительными трёхзначными числами.
+// Напишите программу, которая покажет количество чётных чисел в массиве.
+//[345, 897, 568, 234] -> 2
+
+
+int[] arr = new int [new Random().Next(4, 10)];
+int count = 0;
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(100, 1000);
+    Console.Write($"{arr[i]}, "); 
+    if (arr[i] % 2 == 0) count++;
+}
+Console.Write($"\b\b] -> {count}");
+// Console.Write($"{count}");
---
+++ HW5/Task35/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+//Найдите сумму элементов, стоящих на нечётных позициях.
+
+int[] arr = new int [new Random().Next(4, 10)];
+int sum = 0;
+Console.Write("[");
+for (int i = 0; i < arr.Length; i++)
+{
+    arr[i] = new Random().Next(1, 50);
+    Console.Write($"{arr[i]}, "); 
+    if (i % 2 == 0) sum = sum + arr[i];
+}
+Console.Write($"\b\b] -> {sum}");

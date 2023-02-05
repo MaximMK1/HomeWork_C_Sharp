2dff144426a0316b7559d7278fb4be92546c8356
maxim kovalenko <mk@system-pro.net>
2023-02-05 11:48:59 +0300

    Шестое ДЗ начало

---
+++ HW6/Task41/Program.cs
@@ -0,0 +1,37 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+Console.Write("Введите число 1:  ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 2:  ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 3:  ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 4:  ");
+int d = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 5:  ");
+int e = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите число 6:  ");
+int f = Convert.ToInt32(Console.ReadLine());
+
+int[] arr = {a,b,c,d,e,f};
+
+int PosNumArr(int[] array)
+{
+    int count =0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] > 0) count++;
+    }
+return count;
+}
+
+Console.Write($"Среди введенных чисел  {string.Join(", ", arr)}  положительных {PosNumArr(arr)}");
---
+++ HW6/Task43/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+
+Console.Write("Введите значение b1:  ");
+int k1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение k1:  ");
+int b1 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение b2:  ");
+int k2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите значение k2:  ");
+int b2 = Convert.ToInt32(Console.ReadLine());
+
+// int x;
+// int y;
+
+double PointX(int k1, int b1, int k2, int b2) 
+{
+    // double y;
+    double x = (b2-b1)/(k1-k2); 
+    // y = a1 * x + a2;
+    // return x;
+    //  double y = a1 * x + a2;
+    //  double y2 = c1 * x + c2;
+    //  y1 = y2;
+     return x;
+}
+Console.Write(PointX(b1, k1, b2, k2));

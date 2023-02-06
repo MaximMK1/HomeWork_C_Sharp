9349fc3c91ec9e5df8c1419593bbc9a678f7e1e0
maxim kovalenko <mk@system-pro.net>
2023-02-06 20:54:02 +0300

    Шестое ДЗ и начало седьмого

---
+++ HW7/Task47/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задайте двумерный массив размером mxn,
+//заполненный случайными вещественными числами.
+
+Console.Write("Введите размер m ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите размер n ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+double[,] arr = new double[m,n];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1) ; j++)
+    {
+       arr[i,j] = Math.Round((new Random().NextDouble() * 10), 2); 
+    }
+}
+
+Console.WriteLine("Массив с вещественными элементами");
+PrintArr(arr);
+
+void PrintArr(double[,] array)
+{
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) ; j++)
+        {
+            Console.Write($"{array[i, j],7}");
+        }
+        Console.WriteLine();
+    } 
+}
---
+++ HW7/Task50/Program.cs
@@ -0,0 +1,53 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента
+// в двумерном массиве, и возвращает значение этого элемента или же
+// что такого элемента нет.
+
+
+Console.Write("Введите число строк массива: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число столбцов массива: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите номер строки элемента: ");
+int elementRow = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите номер столбца элемента: ");
+int elementCol = Convert.ToInt32(Console.ReadLine());
+
+int[,] arr = new int[m,n];
+for (int i = 0; i < arr.GetLength(0); i++)
+{
+    for (int j = 0; j < arr.GetLength(1) ; j++)
+    {
+       arr[i,j] = new Random().Next(-9, 10); 
+    }
+}
+
+PrintArr(arr);
+void PrintArr(int[,] arry)
+{
+   for (int i = 0; i < arry.GetLength(0); i++)
+    {
+        for (int j = 0; j < arry.GetLength(1) ; j++)
+        {
+            Console.Write($"{arry[i, j],4}");
+        }
+        Console.WriteLine();
+    } 
+}
+
+int SearchElement(int[,] array, int row, int col)
+{
+   int element = 0; 
+   for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1) ; j++)
+        {
+            if (i==row && j==col) element = array[i, j]; 
+        }
+    } 
+    return element;
+}
+if (SearchElement(arr, elementRow, elementCol) != 0)
+{Console.Write($"Элемент есть: {SearchElement(arr, elementRow, elementCol)}");}
+else
+{Console.Write("Такого элемента нет");}
